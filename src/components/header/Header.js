2fa9234522eff2e98e538abd3fac95164ded154f
Kaijonsson <axel.jonsson@hotmail.com>
2021-09-09 14:07:51 +0200

    fontawesome installed and added icons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./header.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
 function Header() {
   return (
     <div id="headerContainer">
       <div id="appMenu">
-        <p>Å</p>
-        <p>U</p>
-        <p>O</p>
+        <p>
+          <FontAwesomeIcon icon={faUser} className="userIcons" />
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faShoppingCart} className="userIcons" />
+        </p>
       </div>
       <h1 id="headerHeader">Hej På Dig Viner</h1>
     </div>
