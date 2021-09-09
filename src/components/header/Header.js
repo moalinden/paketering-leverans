21c34cd0b11a3f378398c1704afe0469577dc023
Kaijonsson <axel.jonsson@hotmail.com>
2021-09-09 13:14:19 +0200

    basic header finnished

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div id="headerContainer">
+      <div id="appMenu">
+        <p>Å</p>
+        <p>U</p>
+        <p>O</p>
+      </div>
+      <h1 id="headerHeader">Hej På Dig Viner</h1>
+    </div>
+  );
+}
+
+export default Header;
