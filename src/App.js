dac446a21c30d19c4782987f2b25104daa82b207
clartor <clara.tornblom@gmail.com>
2021-09-10 17:15:31 +0200

    first element added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import WineBottles from "./components/wineCards/WineBottles";
 
 function App() {
   return (
     <div id="App">
       <Header />
+      <WineBottles />
       <Footer />
     </div>
   );
