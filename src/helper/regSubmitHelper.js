12acacb88b75d7cd908a3fed4699154b0e4cdbcc
TimmieO <timmieoskarsson01@gmail.com>
2021-09-13 15:54:02 +0200

    Added helper file to call API for register

--- src/helper/regSubmitHelper.js
+++ src/helper/regSubmitHelper.js
@@ -1,11 +1,30 @@
+import fetchHelper from './fetchHelper';
+
 export default async function RegSubmitHelper(data) {
+  console.log(data.username)
 
   //Check in regInfo state object if find Ok that == false
   if(ValidateAllInput(data, false) == true){
     return;
   }
 
+  const dataVal = {
+    username: data.username.val,
+    firstname: data.firstname.val,
+    lastname: data.lastname.val,
+    email: data.email.val,
+    password: data.password.val
+  }
 
+const settings = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(dataVal)
+  }
+
+  let result = await fetch('/api/addUser', settings);
+
+  return result;
 }
 
 function ValidateAllInput(obj, value) {
