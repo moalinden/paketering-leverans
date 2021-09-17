b0a483bd6c00595d1133659f8952b6a4aacf384a
Kaijonsson <axel.jonsson@hotmail.com>
2021-09-17 15:09:32 +0200

    ! removed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import WineBottles from "./components/wineCards/WineBottles";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Login from "./components/login/Login";
-import Register from "./components/register/Register"
+import Register from "./components/register/Register";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -31,7 +31,7 @@ function App() {
     newFetch();
   }, []);
 
-  if (!loading) {
+  if (loading) {
     return (
       <div id="app">
         <Loading />
