608d13324fe1717b8d042d373777c4c07f113b91
clartor <clara.tornblom@gmail.com>
2021-09-17 13:46:37 +0200

    bugfix

--- src/components/wineCards/WineBottles.js
+++ src/components/wineCards/WineBottles.js
@@ -41,63 +41,60 @@ function WineBottles() {
   return (
     <div className="container">
 
-
-        <div id="navbar">
-            <Col >
-                <Row className="justify-content-md-center">
-                    <Nav variant="tabs" defaultActiveKey="/App"  id="navbar" >
-                        {/* <h2>MENU</h2> */}
-                        <Nav.Item>
-                            <Nav.Link href="/App" id="navLink">
-                                Red Wine
-                            </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/App" id="navLink">
-                                White Wine
-                            </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/App" id="navLink">
-                                Sparkling Wine
-                            </Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Row>
-            </Col>
-  </div>
-
-    <div id="systembolaget">
-      <div className="row">
-        {products != undefined ? products.map((wine, index) => (
-          <div className="col-1" id="wineBox" key={index}>
-            <div id="bild">
-              {/* {console.log(wine)} */}
-              <img src={wine.imageUrl} alt="wine and dinee" id="winePic"></img>
-            </div>
-            <div id="wineFacts">
-              <h3>{wine.name}</h3>
-              <p>{wine.description}</p>
-              <p>{wine.price} kr</p>
-              <button id="wishknapp" onCLick={() => saveToWishList()}>♡</button>
+      <div id="navbar">
+        <Col >
+          <Row className="justify-content-md-center">
+            <Nav variant="tabs" defaultActiveKey="/App" id="navbar" >
+              {/* <h2>MENU</h2> */}
+              <Nav.Item>
+                <Nav.Link href="/App" id="navLink">
+                  Red Wine
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/App" id="navLink">
+                  White Wine
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/App" id="navLink">
+                  Sparkling Wine
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Row>
+        </Col>
+      </div>
+      <div id="systembolaget">
+        <div className="row">
+          {products != undefined ? products.map((wine, index) => (
+            <div className="col-1" id="wineBox" key={index}>
+              <div id="bild">
+                {/* {console.log(wine)} */}
+                <img src={wine.imageUrl} alt="wine and dinee" id="winePic"></img>
+              </div>
+              <div id="wineFacts">
+                <h3>{wine.name}</h3>
+                <p>{wine.description}</p>
+                <p>{wine.price} kr</p>
+                <button id="wishknapp" onCLick={() => saveToWishList()}>♡</button>
                 {/* <button id="wishknapp" onClick={() => removeWishList()}>remove</button> */}
                 <div id="cartButtons">
                   <button
                     placeholder="add to cart"
                     onClick={() => addToCart(wine)}
                     id="cartKnapp"
-                  >
+                    >
                     Add to Cart
                   </button>
                   <button onClick={() => decrementCart(wine)} id="cartKnapp">
                     Delete
                   </button>
+                </div>
+              </div>
             </div>
-          </div>
-        )) :null }
-        {/* {handleGetJson()} */}
-        {/* {console.log(bottles)} */}
-
+                    )) :null }
+        </div>
       </div>
     </div>
   );
