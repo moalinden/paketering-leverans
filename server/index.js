4e00b34491cedcb8a3939fac7e3275d7bdf054c3
TimmieO <timmieoskarsson01@gmail.com>
2021-09-17 16:15:18 +0200

    Fixed Register and Login

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-//FIX POST WITH ? (EASIER)
-//WHEN THAT WORKS, GO AND DO SELECT
-
 //Require
 const path = require("path");
 const express = require("express");
 const CryptoJS = require("crypto-js");
-
+const bodyParser = require('body-parser')
 const app = express();
+
+app.use(bodyParser.json())
 
 const sqlite3 = require("sqlite3").verbose();
 
@@ -58,49 +57,9 @@ app.get("/api/products", (req, res) => {
   });
 });
 
-//Add new user
-
-// Get/read all products
-app.get("/api/getUser/:info", (req, res) => {
-  if (req.params.info == "email") {
-    db.all(
-      "SELECT COUNT(id) AS userCount FROM user WHERE email = ?",
-      [req.body.value],
-      (err, rows) => {
-        if (err) {
-          res.status(400).json({ error: err.message });
-          return;
-        }
-        res.status(200).json({ rows });
-      }
-    );
-  }
-});
-
-// Get one product by id
-app.get("/api/products", (req, res) => {
-  db.all("SELECT * FROM products", (err, rows, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-
-    return res.json({ products: rows });
-  });
-});
-
-// Get one product by name
-app.get("/api/products/byName/:name", (req, res) => {
-  let stmt = db.prepare(`
-    SELECT *
-    FROM products
-    WHERE name = :name
-  `);
-  res.json(stmt.all({ name: req.params.name }));
-});
-
 // Create a new product
 app.post("/api/addUser", (req, res) => {
+
   let data = req.body;
 
   //Create SALT auth in auth_str
@@ -128,122 +87,92 @@ app.post("/api/addUser", (req, res) => {
       hash_pwd,
       auth_str,
     ],
-    function (err) {
-      db.run(
-        `INSERT INTO user(username, firstname, lastname, email, password) VALUES(?,?,?,?,?)`,
-        [
-          data.username,
-          data.firstname,
-          data.lastname,
-          data.email,
-          data.password,
-        ],
-        function (err) {
-          if (err) {
-            return console.log(err.message);
-          }
-          // get the last insert id
-        }
-      );
+    (err, rows) => {
+      if(err){
+        res.status(400).json({ message: "Error" });
+        return;
+      }
+      else{
+        res.json({ message: "Welcome!" });
+      }
     }
-  );
+  )
+})
 
   //Login user
-  app.post("/api/loginUser", (req, res) => {
-    let data = req.body;
+app.post("/api/loginUser", (req, res) => {
+  let data = req.body;
 
-    //Check if auth exist (User exist)
-    db.all(
-      "SELECT auth FROM user WHERE username = ?",
-      [data.username],
-      (err, rows, result) => {
-        if (err) {
-          res.status(400).json({ error: err.message });
-          return;
-        }
-
-        //If exist, get auth and encrypt with SHA256
-        if (rows.length > 0) {
-          let auth_str = rows[0].auth;
-          let pwd = data.password;
-          let auth =
-            "adb6f118edd6ca21cd88c2709b5b395266c0b3d71bd3c55bac875a31017c29fa";
-          let hash_pwd = CryptoJS.SHA256(auth + auth_str + pwd).toString(
-            CryptoJS.enc.Hex
-          );
-
-          let sql =
-            "SELECT id, username FROM user WHERE username = ? and password = '" +
-            hash_pwd +
-            "'";
-
-          //Perform sql query to check if username and password matches the one in DB
-          db.all(sql, [data.username], (err, rows, result) => {
-            if (err) {
-              res.status(400).json({ error: err.message });
-              return;
-            } else {
-              //If matches = logged in
-              if (rows.length > 0) {
-                //User exists
-                return res.json({ message: "Logged in" });
-              } else {
-                return res.json({ message: "Wrong info" });
-              }
-            }
-          });
-        }
-      }
-    );
-  });
-
-  // Update/change a product
-  app.put("/api/products/:id", (req, res) => {
-    // Only change fields that are mentioned in req.body
-    // (so this query is built very dynamically)
-    let query = `
-    UPDATE products
-    SET ${Object.keys(req.body)
-      .map((x) => `${x} = :${x}`)
-      .join(", ")}
-    WHERE id = :id
-  `;
-    console.log(query);
-    // Now make a prepared statment and run it
-    let stmt = db.prepare(query);
-    res.json(stmt.run({ id: req.params.id, ...req.body }));
-  });
-
-  // Delete a product
-  app.delete("/api/products/:id", (req, res) => {
-    let stmt = db.prepare(`
-    DELETE FROM products WHERE id = :id
-  `);
-  res.json(stmt.run({ id: req.params.id }));
-});
-
-
-  app.get("/api/changeImage", (req, res) => {
-    db.all(
-      "UPDATE products SET imageUrl = '../src/media/red/la-royal.jpg' WHERE imageUrl = '../media/red/la-royal.jpg'",
-      (err, rows, result) => {
-        if (err) {
-          res.status(400).json({ error: err.message });
-          return;
-        }
-      }
-    );
-  });
-
-  app.get("/api/selectImage", (req, res) => {
-    db.all("SELECT * FROM products", (err, rows, result) => {
+  //Check if auth exist (User exist)
+  db.all(
+    "SELECT auth FROM user WHERE username = ?",
+    [data.username],
+    (err, rows, result) => {
       if (err) {
         res.status(400).json({ error: err.message });
         return;
       }
-      console.log(rows);
-    });
-  });
+
+      //If exist, get auth and encrypt with SHA256
+      if (rows.length > 0) {
+        let auth_str = rows[0].auth;
+        let pwd = data.password;
+        let auth =
+          "adb6f118edd6ca21cd88c2709b5b395266c0b3d71bd3c55bac875a31017c29fa";
+        let hash_pwd = CryptoJS.SHA256(auth + auth_str + pwd).toString(
+          CryptoJS.enc.Hex
+        );
+
+        let sql =
+          "SELECT id, username FROM user WHERE username = ? and password = '" +
+          hash_pwd +
+          "'";
+
+        //Perform sql query to check if username and password matches the one in DB
+        db.all(sql, [data.username], (err, rows, result) => {
+          if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+          } else {
+            //If matches = logged in
+            if (rows.length > 0) {
+              //User exists
+              res.json({ message: "Logged in" });
+            }
+              res.json({ message: "Wrong info" });
+          }
+        });
+      }
+      else{
+        res.json({ message: "No user with that username found!" });
+      }
+    }
+  );
 });
+
+//Dev purpose only REMOVE BEFORE ACTIVATE
+app.get('/api/clearTable', (req, res) => {
+  db.all("DELETE FROM user", [], (err, rows, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    console.log("HAGASFAS", rows);
+
+    return res.json(true);
+  });
+})
+app.get('/api/getUserTable', (req, res) => {
+  db.all("SELECT * FROM user", [], (err, rows, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    console.log("HAGASFAS", rows);
+
+    return res.json(true);
+  });
+})
+
 // start the web server
 app.listen(4000, () => console.log("Listening on port 4000"));
