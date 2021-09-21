bb1ba2b3fa2070d4021d484397e73c26392c6322
pakorn <pakorn_00@hotmail.com>
2021-09-21 16:48:54 +0200

    strullade lite med fixade det

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -65,57 +65,9 @@ function Header() {
           </Col>
         </Row> 
         
-        {/* <Row
-          md="auto"
-          className="justify-content-md-center"
-          style={{ backgroundColor: "#bf8596", color: "#F3E7DA" }}
-        >
-          <Col style={{ position:"absolute", top: "120px" }}>
-            <h1>Hej På Dig Viner</h1>
-          </Col>
-        </Row> */}
       </Container>
     </>
 
-    <Container fluid style={{ backgroundColor: "#bf8596", height:"200px" }}>
-      <Row md="auto" className="justify-content-md-center xs-2">
-      <Col>
-        <Link to="/" style={{ color: "#F3DAC6" }}>
-          <FontAwesomeIcon icon={faWineBottle} className="userIcons" />
-          </Link>
-        </Col>
-        <Col>
-        <Link to="/cart" style={{ color: "#F3DAC6" }}>
-          <FontAwesomeIcon icon={faShoppingCart} className="userIcons" />
-            <Countindicator />
-          </Link>
-        </Col>
-        <Col>
-        <Link to="/Register" style={{ color: "#F3DAC6" }}>
-          <FontAwesomeIcon icon={faPenFancy} className="userIcons xs-2" />
-          </Link>
-        </Col>
-        <Col>
-        <Link to="/Login" style={{ color: "#F3DAC6" }}>
-          <FontAwesomeIcon icon={faUser} className="userIcons xs-2" />
-          </Link>
-        </Col>
-        <Col>
-        <Link to="/About" style={{ color: "#F3DAC6" }}>
-          <FontAwesomeIcon icon={faInfo} className="userIcons" />
-          </Link>
-        </Col>
-      </Row>
-
-      <Row
-        md="auto"
-        className="justify-content-md-center"
-        style={{ backgroundColor: "#bf8596", color: "#F3E7DA" }}>
-        <Col style={{ position:"absolute", top: "120px" }}>
-          <h1>Hej På Dig Viner</h1>
-        </Col>
-      </Row>
-    </Container>
   );
 }
 
