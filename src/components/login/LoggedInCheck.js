73843448011ca98ee9a014b3c699a9e3f975a16e
TimmieO <timmieoskarsson01@gmail.com>
2021-09-23 05:14:43 +0200

    Added extra check for storage

--- src/components/login/LoggedInCheck.js
+++ src/components/login/LoggedInCheck.js
@@ -1,9 +1,25 @@
 export function isLoggedIn(){
-  let storageVal = localStorage.getItem("logged_in");
+  let userLoggedIn = localStorage.getItem("logged_in");
+  let tokenStorageVal = localStorage.getItem("user_session");
 
-  if(storageVal == null || storageVal == "false"){
+  //If userLoggedIn exist but not tokenStorageVal
+  //Remove "logged_in" to make user login again
+  if(userLoggedIn == 'true' && tokenStorageVal == null){
+    localStorage.clear("logged_in");
     return false;
-  }else if(storageVal == "true"){
+  }
+
+  //If tokenStorageVal exist but not storageVal
+  //Create new localStorage set "logged_in" to true
+  if(tokenStorageVal != null && userLoggedIn == null){
+    localStorage.setItem("logged_in", true);
+    return true;
+  }
+
+  if(userLoggedIn == null || userLoggedIn == "false"){
+    return false;
+  }
+  if(userLoggedIn == "true"){
     return true;
   }
 }
