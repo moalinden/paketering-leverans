c8613b3c3a516892b5efe4c88bad92b3e02e8e85
Kaijonsson <axel.jonsson@hotmail.com>
2021-09-23 10:57:26 +0200

    removed unused and fixed routes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,92 +1,101 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useSelector } from "react-redux";
-import { faUser, faShoppingCart, faInfo, faWineBottle, faPenFancy } from "@fortawesome/free-solid-svg-icons";
-import { Container, Row, Col, Nav, Navbar, NavDropdown, form, input } from "react-bootstrap";
-import {isLoggedIn} from '../login/LoggedInCheck';
+import {
+  faUser,
+  faShoppingCart,
+  faInfo,
+  faWineBottle,
+} from "@fortawesome/free-solid-svg-icons";
+import { Container, Row, Col, Nav } from "react-bootstrap";
+import { isLoggedIn } from "../login/LoggedInCheck";
 
 import Countindicator from "./CountIndicator";
-import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
 
-
-import './header.css';
+import "./header.css";
 
 function Header() {
+  const [loggedIn] = useState(isLoggedIn());
   const history = useHistory();
 
-  const [loggedIn, setLoggedIn] = useState(isLoggedIn());
+  const navTo = (location) => {
+    history.push(location);
+  };
 
   return (
-
     <>
       <div className="upper-nav">
         <h1 style={{ fontSize: "82px" }}>Hello Wines</h1>
       </div>
 
-    {console.log(loggedIn)}
       <div className="navbar-font">
-      <Container fluid style={{ backgroundColor: "#bf8596", textAlign:"center", height:'110px'}}>
-
-        <Row md="auto" className="justify-content-md-center xs-2">
-        <Col>
-          <Nav.Link href="/" style={{ color: "#F3DAC6" }}>
-            <p>Wines</p>
-            <FontAwesomeIcon icon={faWineBottle} className="userIcons" />
-            </Nav.Link>
-          </Col>
-
-          <Col>
-          <Nav.Link href="/Cart" style={{ color: "#F3DAC6" }}>
-          <p>Cart</p>
-            <FontAwesomeIcon icon={faShoppingCart} className="userIcons" />
-            <Countindicator />
-            </Nav.Link>
-          </Col>
-
-          { loggedIn == false ?
-            [
-          // <Col>
-          // <Nav.Link href="/Register" style={{ color: "#F3DAC6" }}>
-          // <p>Sign Up</p>
-          //   <FontAwesomeIcon icon={faPenFancy} className="userIcons xs-2" />
-          //   </Nav.Link>
-          // </Col>
-            ,
-          <Col>
-          <Nav.Link href="/Login" style={{ color: "#F3DAC6" }}>
-          <p>Sign In</p>
-            <FontAwesomeIcon icon={faUser} className="userIcons xs-2" />
-            </Nav.Link>
-          </Col>
-            ]
-            :
+        <Container
+          fluid
+          style={{
+            backgroundColor: "#bf8596",
+            textAlign: "center",
+            height: "110px",
+          }}
+        >
+          <Row md="auto" className="justify-content-md-center xs-2">
             <Col>
-              <Nav.Link href="/logout" style={{ color: "#F3DAC6" }}>
-                <p>Log Out</p>
-                <FontAwesomeIcon icon={faUser} className="userIcons xs-2" />
+              <Nav.Link onClick={() => navTo("/")} style={{ color: "#F3DAC6" }}>
+                <p>Wines</p>
+                <FontAwesomeIcon icon={faWineBottle} className="userIcons" />
               </Nav.Link>
             </Col>
-            }
 
-          <Col>
-          <Nav.Link href="/About" style={{ color: "#F3DAC6" }}>
-          <p>About</p>
-            <FontAwesomeIcon icon={faInfo} className="userIcons" />
-            </Nav.Link>
-          </Col>
-        </Row> 
-        
-      </Container>
+            <Col>
+              <Nav.Link
+                onClick={() => navTo("/Cart")}
+                style={{ color: "#F3DAC6" }}
+              >
+                <p>Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart} className="userIcons" />
+                <Countindicator />
+              </Nav.Link>
+            </Col>
+
+            {loggedIn == false ? (
+              [
+                ,
+                <Col>
+                  <Nav.Link
+                    onClick={() => navTo("/Login")}
+                    style={{ color: "#F3DAC6" }}
+                  >
+                    <p>Sign In</p>
+                    <FontAwesomeIcon icon={faUser} className="userIcons xs-2" />
+                  </Nav.Link>
+                </Col>,
+              ]
+            ) : (
+              <Col>
+                <Nav.Link
+                  onClick={() => navTo("/logout")}
+                  style={{ color: "#F3DAC6" }}
+                >
+                  <p>Log Out</p>
+                  <FontAwesomeIcon icon={faUser} className="userIcons xs-2" />
+                </Nav.Link>
+              </Col>
+            )}
+
+            <Col>
+              <Nav.Link
+                onClick={() => navTo("/About")}
+                style={{ color: "#F3DAC6" }}
+              >
+                <p>About</p>
+                <FontAwesomeIcon icon={faInfo} className="userIcons" />
+              </Nav.Link>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </>
-
   );
 }
 
-
 export default Header;
-
-// color: #F3E7DA;
-
--- src/components/wineCards/WineBottles.js
+++ src/components/wineCards/WineBottles.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./wineBottles.css";
 import WineList from "./WineList";
 
@@ -8,32 +8,15 @@ import { useSelector } from "react-redux";
 import Loading from "../loading/Loading";
 
 function WineBottles() {
-  // const dispatch = useDispatch();
   const productsState = useSelector((state) => state.storeSlice);
   const products = productsState.storedProducts;
-  const [wineCat, setWineCat] = useState("")
-  // console.log("outside function: ", productsState);
-
-
-  // const saveToWishList = () => {
-  //   const wishList = productsState.products;
-  //   const keyGen = Math.random() * 1000;
-  //   localStorage.setItem(keyGen, wishList);
-  //   dispatch(storeWishList(keyGen));
-  // };
-  // const removeWishList = (key) => {
-  //   dispatch(removeWishList(key));
-  //   const keyToRemove = productsState.keyToRemove;
-  //   localStorage.removeItem(keyToRemove);
-  // };
+  const [wineCat, setWineCat] = useState("");
 
   const chooseWine = (cat) => {
-    setWineCat(cat)
-    console.log(cat)
-    
-  }
+    setWineCat(cat);
+    console.log(cat);
+  };
 
-  
   if (!products) {
     return <Loading />;
   } else {
@@ -43,7 +26,11 @@ function WineBottles() {
           <Col>
             <Row className="justify-content-md-center">
               <Nav variant="tabs" id="navbar">
-                <Button id="navLink" onClick={() => chooseWine("")} style={{marginRight:'19px'}}>
+                <Button
+                  id="navLink"
+                  onClick={() => chooseWine("")}
+                  style={{ marginRight: "19px" }}
+                >
                   All Wine
                 </Button>
                 <Button id="navLink" onClick={() => chooseWine("red")}>
@@ -61,7 +48,7 @@ function WineBottles() {
         </div>
         <div id="systembolaget">
           <div className="row">
-            <WineList data={wineCat}/>
+            <WineList data={wineCat} />
           </div>
         </div>
       </div>
